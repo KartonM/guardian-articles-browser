b994e33c5060e758de7cfac056cfca43def622d0
Marcin Kozubek <mkozubek@protonmail.com>
2021-03-17 23:15:25 +0100

    Add no bookmarked articles text

--- src/screens/BookmarkedArticlesScreen.js
+++ src/screens/BookmarkedArticlesScreen.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, Text} from 'react-native';
 import ArticlesList from '../components/ArticlesList';
 import {useSelector} from 'react-redux';
 import useTheme from '../hooks/useTheme';
 import TopBar from '../components/TopBar';
 
-function BookmarkedArticlesScreen() {
+const BookmarkedArticlesScreen = () => {
   const [theme] = useTheme();
   const bookmarkedArticles = useSelector((state) => state.bookmarkedArticles);
 
@@ -14,18 +14,29 @@ function BookmarkedArticlesScreen() {
       <TopBar />
       <View
         style={[styles.container, {backgroundColor: theme.colors.background}]}>
-        <ArticlesList
-          allArticles={bookmarkedArticles}
-          mainSection={{id: '', name: 'Bookmarked articles'}}
-        />
+        {bookmarkedArticles.length > 0 ? (
+          <ArticlesList
+            allArticles={bookmarkedArticles}
+            mainSection={{id: '', name: 'Bookmarked articles'}}
+          />
+        ) : (
+          <Text style={[styles.noArticlesText, {color: theme.colors.text}]}>
+            Bookmarked articles will be listed here
+          </Text>
+        )}
       </View>
     </>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  noArticlesText: {
+    fontSize: 18,
+    textAlign: 'center',
+    marginTop: 48,
   },
 });
 
